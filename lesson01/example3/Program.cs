1833e1ad4a45850c30c6b3d990af22c5da90540f
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-04-25 23:17:29 +0500

    add example 3

---
+++ lesson01/example3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число - ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("Это четное число!");
+}
+else
+{
+    Console.Write("Это не четное число");
+}
