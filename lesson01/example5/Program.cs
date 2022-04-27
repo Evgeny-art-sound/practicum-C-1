17736cb5960dbee1b2d509f617dafa87eff1ee42
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-04-27 14:48:56 +0500

    add exaampple 5

---
+++ lesson01/example5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+if (a < 0)
+{
+    a = a * -1;
+}
+if (a > 99 && a < 1000)
+{
+    a = a / 10;
+    Console.Write("Вторая цифра трехзначного числа: ");
+    Console.WriteLine(a % 10);
+}
+else
+{
+    Console.Write("Это не 3х значное число!");
+}
