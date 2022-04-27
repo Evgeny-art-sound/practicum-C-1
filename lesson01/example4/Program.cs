b1bea8859d01722771e6aabd19845816b591990b
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-04-27 14:14:43 +0500

    add example 4

--- lesson01/example4/Program.cs
+++ lesson01/example4/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("Введите число - ");
+int a = int.Parse(Console.ReadLine());
+int b = 1;
+Console.WriteLine("Список всех четных чисел от 1 до введенного числа: ");
+while (b <= a)
+{
+    if (b % 2 == 0)
+    {
+        Console.Write( b); 
+        Console.Write (", ");
+    }
+    b++;
+}
