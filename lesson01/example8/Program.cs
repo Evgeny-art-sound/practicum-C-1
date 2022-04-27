4ed28663673b3b4eb70e4997992d613f5fb07ff7
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-04-27 19:48:50 +0500

    add example 8

---
+++ lesson01/example8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите делимое: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите делитель: ");
+int b = int.Parse(Console.ReadLine());
+if (a % b == 0)
+{
+Console.WriteLine("Числа кратны ");
+}
+else{
+    Console.WriteLine ("Числа не кратны ");
+}
