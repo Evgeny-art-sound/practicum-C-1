7d5d991407ef59ba946bfdafad9aed5bf7b66e5f
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-04-27 21:50:46 +0500

    add example 9

---
+++ lesson01/example9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x < 0)
+{
+    x = x * -1;
+}
+if (x < 100)
+
+{
+    Console.WriteLine("Третьей цифры в числе нет!");
+
+}
+else
+{
+    while (x > 999)
+    {
+        x = x / 10;
+    }
+    Console.WriteLine($"Третья цифра введенного числа: {x % 10}");
+}
